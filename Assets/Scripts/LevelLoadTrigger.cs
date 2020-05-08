dcc734ff11bb0c24d05b514e0aedf138a216dbad
request <iamrequest@protonmail.com>
2020-05-08 01:58:47 -0400

    Added sandbox scene, level loader triggers

---
+++ Assets/Scripts/LevelLoadTrigger.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class LevelLoadTrigger : MonoBehaviour {
+    public string levelName;
+    private void OnTriggerEnter(Collider other) {
+        SteamVR_LoadLevel.Begin(levelName);
+    }
+}
