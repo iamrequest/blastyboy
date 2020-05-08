416d0bd1851eca7ae4782b71ebf2b620f9be040b
request <iamrequest@protonmail.com>
2020-05-07 22:43:59 -0400

    Initial level design

---
+++ Assets/Scripts/TechDoor.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class TechDoor : MonoBehaviour {
+    Animator animator;
+    public Transform proximityTarget;
+    public Vector3 doorCenterPosition;
+    public float doorOpenDistance, doorCloseDistance;
+
+    public bool isDoorOpenOnStart;
+    private bool m_isDoorOpen;
+    public bool isDoorOpen {
+        get {
+            return m_isDoorOpen;
+        }
+        set {
+            if (isDoorLocked) return;
+            animator.SetBool("isDoorOpen", !value);
+            m_isDoorOpen = value;
+        }
+    }
+
+    private bool m_isDoorLocked;
+    public bool isDoorLocked {
+        get {
+            return m_isDoorLocked;
+        }
+        set {
+            m_isDoorLocked = value;
+            lockIcon.SetActive(value);
+        }
+    }
+    public GameObject lockIcon;
+
+    private void Start() {
+        animator = GetComponent<Animator>();
+        animator.StopPlayback();
+
+        m_isDoorLocked = lockIcon.activeInHierarchy;
+        m_isDoorOpen = isDoorOpenOnStart;
+    }
+
+    private void Update() {
+        if (proximityTarget == null) {
+            proximityTarget = Player.instance.hmdTransform;
+        }
+
+        if (!isDoorLocked) {
+            if (proximityTarget != null) {
+                float distanceToDoor = (proximityTarget.position - doorCenterPosition).magnitude;
+
+                if (isDoorOpen) {
+                    if (distanceToDoor > doorCloseDistance) {
+                        isDoorOpen = false;
+                    }
+                } else {
+                    if (distanceToDoor < doorOpenDistance) {
+                        isDoorOpen = true;
+                    }
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -6,7 +6,11 @@
 	* URP
 	* Animation Rig 0.2.6
 	* TextMeshPro
+	* Probuilder
+	* Snaps (Office, sci-fi)
 * SDK: SteamVR 2.5.0 (sdk 1.8.19)
 * Assets:
 	* Enemy models and animations: Mixamo
 	* Sound: Zapsplat.com
+	* Hologram texture: https://github.com/Brackeys/Shader-Graph-Tutoriags
+	* Skybox: "Starfield Skybox" by Pulsar Bytes
